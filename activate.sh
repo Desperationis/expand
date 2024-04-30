5380c82b4aff33251810dcd593204bc3b6958572
Desperationis <smarttdiego@gmail.com>
2024-04-30 02:34:43 -0400

    Add basic activate.sh

--- activate.sh
+++ activate.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
+#
+expand_bootstrap() {
+	# Navigate to same directory as this script
+	cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+	bash bootstrap/install_python.bash
+	bash bootstrap/install_ansible.bash
+	bash bootstrap/install_pip.bash
+	bash bootstrap/install_venv.bash
+
+	# TODO: Check if venv exists. If it already does just activate it
+
+	python3 -m venv venv
+	. venv/bin/activate
+
+	pip3 install -r requirements.txt
+}
 
 if ! [ "$(id -u)" = "0" ]; then
 	echo -e "\033[1;31mThis script must be directly run as root, not as sudo.\033[0m"
-	exit 1
+else
+	expand_bootstrap
 fi
 
-
-# All operations are done on top of `expand`
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR"
-
-bash bootstrap/install_python.bash
-bash bootstrap/install_ansible.bash
-bash bootstrap/install_pip.bash
-bash bootstrap/install_venv.bash
-
-python3 -m venv venv
-. venv/bin/activate
-
-pip3 install -r requirements.txt
