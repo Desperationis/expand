b88d5ee0a0b58a0dff21085faf57f8deace979a9
Desperationis <fork_important@pm.me>
2024-07-09 01:49:14 -0700

    Revert "Separate nvim to two"
    This reverts commit 744627da1af44b0884630a85bdc95a71632b5e61.

--- data/config/nvim/minimal/init.lua
+++
@@ -1,7 +0,0 @@
--- A lot of help from this post: https://martinlwx.github.io/en/config-neovim-from-scratch/
--- But don't trust post fully! Specifically, file structure. Lazy.nvim loads it itself
-
-require("config.lazy")
-require("config.keymaps")
-require("config.options")
-
